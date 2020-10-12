9827bf9b1fcd606bc727c3be6bfcd2f2befa80b3
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2020-10-12 14:29:31 +0800

    Fix #161

--- install-release.sh
+++ install-release.sh
@@ -32,7 +32,7 @@ curl() {
 
 systemd_cat_config() {
   if systemd-analyze --help | grep -qw 'cat-config'; then
-    systemd-analyze cat-config "$@"
+    systemd-analyze --no-pager cat-config "$@"
   else
     echo "${red}warning: ${green}The systemd version on the current operating system is too low."
     echo "${red}warning: ${green}Please consider to upgrade the systemd or the operating system.${reset}"
