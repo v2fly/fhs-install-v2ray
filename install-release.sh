07a829acf07289a305b28505866f3ae74fb1d0cd
Nicholas Wang <me@nicho1as.wang>
2020-11-25 13:38:44 -0600

    attempt of fix to #182 (#184)
    Co-authored-by: IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>

--- install-release.sh
+++ install-release.sh
@@ -47,8 +47,14 @@ systemd_cat_config() {
 check_if_running_as_root() {
   # If you want to run as another user, please modify $UID to be owned by this user
   if [[ "$UID" -ne '0' ]]; then
-    echo "error: You must run this script as root!"
-    exit 1
+    echo "WARNING: The user currently executing this script is not root. You may encounter the insufficient privilege error."
+    read -r -p "Are you sure you want to continue? [y/n] " cont_without_been_root
+    if [[ x"${cont_without_been_root:0:1}" = x'y' ]]; then
+      echo "Continuing the installation with current user..."
+    else
+      echo "Not running with root, exiting..."
+      exit 1
+    fi
   fi
 }
 
