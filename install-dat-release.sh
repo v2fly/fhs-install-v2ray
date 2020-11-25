fabb32ff4c2be46b46a82e167f78bfa3324c313b
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2020-11-26 03:37:28 +0800

    Apply suggestions from code review

--- install-dat-release.sh
+++ install-dat-release.sh
@@ -29,7 +29,7 @@ curl() {
 check_if_running_as_root() {
   # If you want to run as another user, please modify $UID to be owned by this user
   if [[ "$UID" -ne '0' ]]; then
-    echo "${red}warning: ${green}The user currently executing this script is not root. You may encounter the insufficient privilege error.${reset}"
+    echo "WARNING: The user currently executing this script is not root. You may encounter the insufficient privilege error."
     read -r -p "Are you sure you want to continue? [y/n] " cont_without_been_root
     if [[ x"${cont_without_been_root:0:1}" = x'y' ]]; then
       echo "Continuing the installation with current user..."
